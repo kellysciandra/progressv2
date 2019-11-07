16a75bba8e75eac042f3b0a3c409953d786ec4a2
Kelly Sciandra <kellysciandradev@icloud.com>
2019-11-07 12:14:24 -0500

    woking models

--- app/controllers/api/v1/languages_controller.rb
+++ app/controllers/api/v1/languages_controller.rb
@@ -1,2 +1,47 @@
 class Api::V1::LanguagesController < ApplicationController
+
+    def index
+        @languages = Language.all
+        
+        render json: @languages, status: 200
+    end 
+
+    def show 
+        @language = Language.find(params[:id])
+
+        render json: @language, status: 200
+    end 
+
+    def create
+        @language = Language.create(language_params)
+        
+        if @language.save 
+            render json: @language, status: 201
+        else 
+            render json: {message: "Error, please try again"}, status: 400
+        end 
+    end 
+
+    def update
+        if @language.update(language_params)
+            render json: @language, status: 200
+        else 
+            render json: {message: "Error, please try again"}, status: 400
+        end 
+    end
+
+    def destroy
+        @language.destroy
+        render json: {message: "You have become a master"},  status: 400
+    end 
+
+    private
+
+    def language_params
+        params.permit(:name, :avatar)
+    end
+
+    def set_language
+        @language = Language.find(params[:id])
+    end 
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,30 @@
 class Api::V1::UsersController < ApplicationController
+    
+    def index
+        @users = User.all
+        render json: users, status: 200
+    end 
+
+    def show 
+        render json: @user, status: 200
+    end 
+
+    def create
+        user = User.new(user_params)
+        if user.save 
+            render json: user, status: 201
+        else
+            render json: {message: "Error, please re-enter"}, status: 400
+        end 
+    end
+    
+    private
+
+    def user_params
+        params.permit(:name)
+    end
+
+    def set_user
+        @user = User.find(params[:id])
+    end 
 end
